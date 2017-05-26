a285c367417bcf69ef3d96dbeb8795d82f4ba853
F483 <fabian.barkhau@gmail.com>
2017-05-26 20:19:36 +0200

    added subscription management

--- quasar.go
+++ quasar.go
@@ -2,6 +2,7 @@ package quasar
 
 import (
 	"github.com/btcsuite/btcutil"
+	"sort"
 )
 
 const TTL uint32 = 128       // Hops event can travel before being dropped.
@@ -14,11 +15,12 @@ type Filters [DEPTH][SIZE]byte
 type TopicDigest [20]byte // ripemd160(sha256(topic))
 type PubKey [65]byte      // compressed secp256k1 public key
 type PeerID PubKey        // pubkey to enable authentication/encryption
+type Subscriptions map[string][]chan string
 
 type Event struct {
 	topicDigest *TopicDigest
-	message     *string
-	publishers  *[]PeerID
+	message     string
+	publishers  []PeerID
 	ttl         uint32
 }
 
@@ -31,8 +33,8 @@ func HashTopic(topic string) *TopicDigest {
 func NewEvent(topic string, message string) *Event {
 	return &Event{
 		topicDigest: HashTopic(topic),
-		message:     &message,
-		publishers:  &[]PeerID{},
+		message:     message,
+		publishers:  []PeerID{},
 		ttl:         TTL,
 	}
 }
@@ -55,23 +57,85 @@ type OverlayNetwork interface {
 	ReceivedUpdateChannel() chan Update
 	SendEvent(PeerID, Event)
 	SendUpdate(PeerID, Filters)
+	Start()
+	Stop()
 }
 
 type Quasar struct {
 	network       OverlayNetwork
-	filters       Filters                  // own filters
-	subscriptions map[string][]chan string // topic name -> message channel
-	peers         []Peer                   // known peer filters
+	filters       *Filters           // own filters
+	subscriptions Subscriptions      // topic name -> message chan
+	peers         map[PeerID]Filters // known peer filters
+	// TODO add flags for workers to stop
 }
 
-func (q Quasar) Subscribe(topic string, msgReceiver chan string) {
-
+func NewQuasar(network OverlayNetwork) *Quasar {
+	q := Quasar{
+		network:       network,
+		filters:       new(Filters),
+		subscriptions: Subscriptions{},
+		peers:         map[PeerID]Filters{},
+	}
+	return &q
 }
 
-func (q Quasar) Unsubscribe(topic string, msgReceiver chan string) {
-
+func (q *Quasar) Start() {
+	// TODO make thread safe
+	q.network.Start()
+	// TODO reset worker flags
+	// TODO start process received events worker
+	// TODO start process received updates worker
+	// TODO start propagate filters worker
 }
 
-func (q Quasar) Subscriptions() []string {
-	return nil
+func (q *Quasar) Stop() {
+	// TODO make thread safe
+	q.network.Stop()
+	// TODO set flags for workers to stop
+	// TODO wait for workers to stop?
+}
+
+func (q *Quasar) Subscribe(topic string, msgReceiver chan string) {
+	// TODO make thread safe
+	receivers, ok := q.subscriptions[topic]
+	if ok != true {
+		q.subscriptions[topic] = []chan string{msgReceiver}
+	} else {
+		q.subscriptions[topic] = append(receivers, msgReceiver)
+	}
+}
+
+func (q *Quasar) Unsubscribe(topic string, msgReceiver chan string) {
+	// TODO make thread safe
+
+	receivers, ok := q.subscriptions[topic]
+
+	// remove specific message receiver
+	if ok && msgReceiver != nil {
+		for i, v := range receivers {
+			if v == msgReceiver {
+				receivers = append(receivers[:i], receivers[i+1:]...)
+				q.subscriptions[topic] = receivers
+				break
+			}
+		}
+	}
+
+	// remove subscription key if no specific message
+	// receiver provided or no message receiver remaining
+	if ok && (len(q.subscriptions[topic]) == 0 || msgReceiver == nil) {
+		delete(q.subscriptions, topic)
+	}
+}
+
+func (q *Quasar) Subscriptions() []string {
+	// TODO make thread safe
+	topics := make([]string, len(q.subscriptions))
+	i := 0
+	for topic := range q.subscriptions {
+		topics[i] = topic
+		i++
+	}
+	sort.Strings(topics)
+	return topics
 }
--- quasar_test.go
+++ quasar_test.go
@@ -2,6 +2,7 @@ package quasar
 
 import (
 	"encoding/hex"
+	"reflect"
 	"testing"
 )
 
@@ -17,30 +18,38 @@ type MockOverlayNetwork struct {
 	network MockNetwork
 }
 
-func (mqt MockOverlayNetwork) Id() PeerID {
+func (mqt *MockOverlayNetwork) Id() PeerID {
 	return mqt.id
 }
 
-func (mqt MockOverlayNetwork) ConnectedPeers() []PeerID {
+func (mqt *MockOverlayNetwork) ConnectedPeers() []PeerID {
 	return mqt.network.connections[mqt.id]
 }
 
-func (mqt MockOverlayNetwork) ReceivedEventChannel() chan Event {
+func (mqt *MockOverlayNetwork) ReceivedEventChannel() chan Event {
 	return mqt.network.eventChannels[mqt.id]
 
 }
 
-func (mqt MockOverlayNetwork) ReceivedUpdateChannel() chan Update {
+func (mqt *MockOverlayNetwork) ReceivedUpdateChannel() chan Update {
 	return mqt.network.updateChannels[mqt.id]
 }
 
-func (mqt MockOverlayNetwork) SendEvent(id PeerID, e Event) {
+func (mqt *MockOverlayNetwork) SendEvent(id PeerID, e Event) {
 	mqt.network.eventChannels[id] <- e
 }
 
-func (mqt MockOverlayNetwork) SendUpdate(id PeerID, f Filters) {
+func (mqt *MockOverlayNetwork) SendUpdate(id PeerID, f Filters) {
 	update := Update{peerId: &mqt.id, filters: &f}
 	mqt.network.updateChannels[id] <- update
+}
+
+func (mqt *MockOverlayNetwork) Start() {
+
+}
+
+func (mqt *MockOverlayNetwork) Stop() {
+
 }
 
 func TestNewEvent(t *testing.T) {
@@ -48,13 +57,13 @@ func TestNewEvent(t *testing.T) {
 	if event == nil {
 		t.Errorf("Expected event!")
 	}
-	if *event.message != "test message" {
+	if event.message != "test message" {
 		t.Errorf("Event message not set!")
 	}
 	if event.ttl != TTL {
 		t.Errorf("Event ttl not set!")
 	}
-	if event.publishers == nil && len(*event.publishers) != 0 {
+	if event.publishers == nil && len(event.publishers) != 0 {
 		t.Errorf("Event publishers not set!")
 	}
 	if event.topicDigest == nil {
@@ -87,5 +96,65 @@ func TestHashTopic(t *testing.T) {
 	digest := HashTopic(topic)
 	if *digest != expected {
 		t.Errorf("HashTopic failed!")
+	}
+}
+
+func TestSubscriptions(t *testing.T) {
+	q := NewQuasar(nil)
+
+	a := make(chan string)
+	q.Subscribe("a", a)
+	subs := q.Subscriptions()
+	if !reflect.DeepEqual(subs, []string{"a"}) {
+		t.Errorf("Incorrect subscriptions! ", subs)
+	}
+
+	b1 := make(chan string)
+	q.Subscribe("b", b1)
+	subs = q.Subscriptions()
+	if !reflect.DeepEqual(subs, []string{"a", "b"}) {
+		t.Errorf("Incorrect subscriptions! ", subs)
+	}
+
+	b2 := make(chan string)
+	q.Subscribe("b", b2)
+	subs = q.Subscriptions()
+	if !reflect.DeepEqual(subs, []string{"a", "b"}) {
+		t.Errorf("Incorrect subscriptions! ", subs)
+	}
+
+	c1 := make(chan string)
+	q.Subscribe("c", c1)
+	subs = q.Subscriptions()
+	if !reflect.DeepEqual(subs, []string{"a", "b", "c"}) {
+		t.Errorf("Incorrect subscriptions!", subs)
+	}
+
+	c2 := make(chan string)
+	q.Subscribe("c", c2)
+	subs = q.Subscriptions()
+	if !reflect.DeepEqual(subs, []string{"a", "b", "c"}) {
+		t.Errorf("Incorrect subscriptions!", subs)
+	}
+
+	// test clears all if no receiver provided
+	q.Unsubscribe("c", nil)
+	subs = q.Subscriptions()
+	if !reflect.DeepEqual(subs, []string{"a", "b"}) {
+		t.Errorf("Incorrect subscriptions!", subs)
+	}
+
+	// only clears specific receiver
+	q.Unsubscribe("b", b1)
+	subs = q.Subscriptions()
+	if !reflect.DeepEqual(subs, []string{"a", "b"}) {
+		t.Errorf("Incorrect subscriptions!", subs)
+	}
+
+	// clears key when last receiver removed
+	q.Unsubscribe("b", b2)
+	subs = q.Subscriptions()
+	if !reflect.DeepEqual(subs, []string{"a"}) {
+		t.Errorf("Incorrect subscriptions!", subs)
 	}
 }
